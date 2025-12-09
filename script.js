0e26290103c26fd5ad799bab7777fd792897023e
emirrdev400 <clashemir9@gmail.com>
2025-12-09 18:13:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let email = document.getElementById("email")
 let password = document.getElementById("password")
 let loginbtn = document.getElementById("login-btn")
 let errorMsg = document.getElementById("error-msg")
+let savedEmail = localStorage.getItem("email")
+let savedPassword = localStorage.getItem("password")
+
 
 loginbtn.addEventListener("click", function(){
     let emailValue = email.value; // o anki değeri
@@ -20,6 +23,13 @@ loginbtn.addEventListener("click", function(){
         return;
     }
 
+    // email içinde boşluk varmı
+    if(/\s/.test(emailValue)){ // \s karakter sınıfı whitespace (boşluk karakterleri) demek
+        errorMsg.innerText = ("Geçerli bir email adresi giriniz boşluk var!")
+        return;
+    }
+
+   
 
     // şifre kontrolü
     if(passwordValue.length < 6 ){
@@ -27,7 +37,20 @@ loginbtn.addEventListener("click", function(){
         return; // kodu durdur
     }
 
-    window.location.href = "main.html"; // ana sayfaya atmak için
+     // büyük harf kontrolü
+     if(!/[A-Z]/.test(passwordValue)){
+        errorMsg.innerText = ("En az 1 büyük harf olmalı")
+        return;
+     }
+
+
+    // LOCALDEKİ VERİYLE EŞLEŞTİRME
+    if(emailValue === savedEmail && passwordValue === savedPassword){
+        errorMsg.innerText = "";
+        window.location.href = "main.html";
+    }else{
+        errorMsg.innerText = "Email veya şifre hatalı!";
+    }
 
 } )
 
